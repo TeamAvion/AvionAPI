318d2582406e229a85d0623add4beaacfb25cd3c
TjKenMate <tjkenmate@gmail.com>
2017-05-25 21:20:48 -0400

    Starting the wrench interface/sample implementation

---
+++ src/main/java/org/teamavion/core/Ref.java
@@ -0,0 +1,8 @@
+package org.teamavion.core;
+
+/**
+ * Created by Thor Johansson on 5/23/2017.
+ */
+public class Ref {
+    public static final String MODID = "teamavioncore", VERSION = "0.1_Alpha";
+}
---
+++ src/main/java/org/teamavion/core/TeamAvionCore.java
@@ -0,0 +1,17 @@
+package org.teamavion.core;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Ref.MODID, version = Ref.VERSION)
+public class TeamAvionCore {
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+    }
+}
